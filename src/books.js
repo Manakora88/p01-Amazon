f0d3233509c089129c01f7cadabc0df38b869fa2
Manakora88 <mk.wloczyk@gmail.com>
2023-10-30 13:09:28 +0100

    Amazon best sellers

---
+++ src/books.js
@@ -0,0 +1,44 @@
+import img1 from './img/AmazonBookNr1.jpg'
+import img2 from './img/AmazonBookNr2.jpg'
+import img3 from './img/AmazonBookNr3.jpg'
+import img4 from './img/AmazonBookNr4.jpg'
+import img5 from './img/AmazonBookNr5.jpg'
+import img6 from './img/AmazonBookNr6.jpg'
+export const books = [
+  {
+    author: 'Britney Spears',
+    title: 'The Woman in Me',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Prince Harry The Duke of Sussex',
+    title: 'Spare',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Laura Nowlin',
+    title: 'If He Had Been with Me',
+    img: img3,
+    id: 3,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img4,
+    id: 4,
+  },
+  {
+    author: 'Colleen Hoover',
+    title: 'Ugly Love',
+    img: img5,
+    id: 5,
+  },
+  {
+    author: 'Alex Michaelides',
+    title: 'The Silent Patient',
+    img: img6,
+    id: 6,
+  },
+]
